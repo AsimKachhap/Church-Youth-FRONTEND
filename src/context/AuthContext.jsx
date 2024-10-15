72ecd3c6731ad97c034d8cfd92aa3d87a3663bf6
unknown <asim26k@gmail.com>
2024-10-15 17:14:59 +0530

    Checking isDetailsCompelete

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -82,7 +82,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, loading }}>
+    <AuthContext.Provider value={{ user, setUser, login, logout, loading }}>
       {children}
     </AuthContext.Provider>
   );
